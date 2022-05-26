e955b065f3a86a73b50e8dbe38d92c9c91699771
Cryptobenkei <apuig75@gmail.com>
2022-05-26 07:48:14 +0200

    Build lib

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,4 +3,5 @@ import { JWKInterface } from 'arweave/node/lib/wallet';
 import Standard from './lib/standard';
 import { testWallet, mineBlock } from './utils/local';
 import { initialState } from './utils/state';
-export { Unite, Standard, testWallet, mineBlock, initialState, JWKInterface };
+import { UniteSchemaState, Field, Proposal, Version } from './contracts/types/standardTypes';
+export { Unite, Standard, testWallet, mineBlock, initialState, JWKInterface, UniteSchemaState, Field, Proposal, Version };
