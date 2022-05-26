cc8a2f6b592c8bccee953c8a8a0e5d55e740037b
Cryptobenkei <apuig75@gmail.com>
2022-05-26 07:02:48 +0200

    Fix package.json

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import Unite from './lib/unite';
+import { JWKInterface } from 'arweave/node/lib/wallet';
 import Standard from './lib/standard';
 import { testWallet, mineBlock } from './utils/local';
 import { initialState } from './utils/state';
-export { Unite, Standard, testWallet, mineBlock, initialState };
+export { Unite, Standard, testWallet, mineBlock, initialState, JWKInterface };
