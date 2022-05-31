8607b0df621b073e6db167b82fb70434c5f68139
Cryptobenkei <apuig75@gmail.com>
2022-05-31 08:17:38 +0200

    ci: initialize more wallets in npm run dev

--- src/tools/dev.ts
+++ src/tools/dev.ts
@@ -7,17 +7,24 @@ import { Unite, Standard } from "../index";
 let arlocal: Arlocal;
 let unite: Unite;
 
-const main = async () => {
-  arlocal = new Arlocal(1984, false);
-  await arlocal.start();
-  unite = await Unite.init("localhost");
-  const walletFile = path.join(__dirname, "wallets", "wallet_test_1.json");
+const prepareWallet = async (walletName: string): Promise<JWKInterface> => {
+  const walletFile = path.join(__dirname, "wallets", walletName);
   const wallet: JWKInterface = JSON.parse(
     fs.readFileSync(walletFile).toString()
   );
   const walletAddress = await unite.getAddress(wallet);
 
   await unite.arweave.api.get(`/mint/${walletAddress}/1000000000000000`);
+  return wallet; 
+};
+
+const main = async () => {
+  arlocal = new Arlocal(1984, false);
+  await arlocal.start();
+  unite = await Unite.init("localhost");
+  const wallet = await prepareWallet("wallet_test_1.json");
+  await prepareWallet("wallet_test_2.json");
+  await prepareWallet("wallet_test_3.json");
   const standard: Standard = await unite.deployStandard(
     wallet,
     "Base NFT",
