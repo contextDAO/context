2f7a41599eb7221257ca9bd3c0638a0d921aff98
Cryptobenkei <cryptobenkei@protonmail.com>
2022-06-22 08:08:48 +0200

    feat: addContributor + test

--- dist/schemas/getSchema.d.ts
+++
@@ -1,8 +0,0 @@
-import { SchemaState, UniteContext } from "../types/types";
-/**
- * getSchema
- *
- * @param {UniteContext} context
- * @param {string} id - Title of the schema
- */
-export default function getSchema(context: UniteContext, id: string): Promise<SchemaState>;
